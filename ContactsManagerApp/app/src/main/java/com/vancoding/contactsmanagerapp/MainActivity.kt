d6d8e3e433bec42e68745fddb2f2e95d51bf04f9
Joshlanuevo <lanuevo68@gmail.com>
2024-06-29 18:03:02 +0800

    modify the main activity

--- ContactsManagerApp/app/src/main/java/com/vancoding/contactsmanagerapp/MainActivity.kt
+++ ContactsManagerApp/app/src/main/java/com/vancoding/contactsmanagerapp/MainActivity.kt
@@ -5,8 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vancoding.contactsmanagerapp.databinding.ActivityMainBinding
+import com.vancoding.contactsmanagerapp.db.ContactDb
+import com.vancoding.contactsmanagerapp.repository.ContactRepository
+import com.vancoding.contactsmanagerapp.viewmodel.ContactViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+    private lateinit var contactViewModel: ContactViewModel;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // ROOM Database
+        val dao = ContactDb.getInstance(applicationContext).contactDao;
+        val repository = ContactRepository(dao);
+
+        // View Model
+        contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java);
+
+        binding.contactViewModel = contactViewModel;
+
+        // use this: LiveData and Data Binding Integration
+        binding.lifecycleOwner = this;
+
+        initReclycerView();
+    }
+
+    private fun initReclycerView() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this);
+        DisplayUsersList();
+    }
+
+    private fun DisplayUsersList() {
+        contactViewModel.contacts.observe(this, Observer {
+
+        })
     }
 }
