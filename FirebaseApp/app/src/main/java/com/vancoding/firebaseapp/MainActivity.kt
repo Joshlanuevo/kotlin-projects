6e93cf429ae6c1cf8736f5605ae5aeb2e0a766f3
Joshlanuevo <lanuevo68@gmail.com>
2024-07-10 14:03:59 +0800

    read data from firebase

--- FirebaseApp/app/src/main/java/com/vancoding/firebaseapp/MainActivity.kt
+++ FirebaseApp/app/src/main/java/com/vancoding/firebaseapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.vancoding.firebaseapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +25,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val textView: TextView = findViewById(R.id.textView);
+
         // Real Time Database Reference
         // https://fir-kotlin-e57f6-default-rtdb.firebaseio.com/
         database = Firebase.database.reference
 
         // Write Data to Firebase
         database.child("price").setValue("1940 PHP")
+
+        // Reading Data from firebase
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val gold_price = snapshot.value;
+                textView.text = gold_price.toString();
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        }
+
+        database.child("price").addValueEventListener(postListener);
     }
 }
