04e5fb65005fa61c6e0aa53a9bfab0b8a1667afb
Joshlanuevo <lanuevo68@gmail.com>
2024-07-10 14:15:39 +0800

    writing custom objects to firebase

--- FirebaseApp/app/src/main/java/com/vancoding/firebaseapp/MainActivity.kt
+++ FirebaseApp/app/src/main/java/com/vancoding/firebaseapp/MainActivity.kt
@@ -25,27 +25,37 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val textView: TextView = findViewById(R.id.textView);
-
         // Real Time Database Reference
         // https://fir-kotlin-e57f6-default-rtdb.firebaseio.com/
         database = Firebase.database.reference
 
-        // Write Data to Firebase
-        database.child("price").setValue("1940 PHP")
+        // Writing custom objects to firebase
+        val user1 = User("Ivan", "1234");
+        database.child("Users").setValue(user1);
 
-        // Reading Data from firebase
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val gold_price = snapshot.value;
-                textView.text = gold_price.toString();
-            }
 
-            override fun onCancelled(error: DatabaseError) {
 
-            }
-        }
-
-        database.child("price").addValueEventListener(postListener);
+//        val textView: TextView = findViewById(R.id.textView);
+//
+//        // Real Time Database Reference
+//        // https://fir-kotlin-e57f6-default-rtdb.firebaseio.com/
+//        database = Firebase.database.reference
+//
+//        // Write Data to Firebase
+//        database.child("price").setValue("1940 PHP")
+//
+//        // Reading Data from firebase
+//        val postListener = object : ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                val gold_price = snapshot.value;
+//                textView.text = gold_price.toString();
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//
+//            }
+//        }
+//
+//        database.child("price").addValueEventListener(postListener);
     }
 }
