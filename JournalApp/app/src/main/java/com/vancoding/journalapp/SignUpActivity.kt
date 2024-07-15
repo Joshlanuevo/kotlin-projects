0b13f0ebac8c842ed861796997a12f56e3a94f1c
Joshlanuevo <lanuevo68@gmail.com>
2024-07-15 16:49:15 +0800

    initialize firebase authentication

--- JournalApp/app/src/main/java/com/vancoding/journalapp/SignUpActivity.kt
+++ JournalApp/app/src/main/java/com/vancoding/journalapp/SignUpActivity.kt
@@ -1,20 +1,76 @@
 package com.vancoding.journalapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.vancoding.journalapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
+
+    companion object {
+        public const val TAG = "Firebase-Auth"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.accSignUpButton.setOnClickListener {
+            createUser()
+        }
+
+    }
+
+    private fun createUser() {
+        val email = binding.emailCreate.text.toString()
+        val password = binding.passwordCreate.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    // When initializing your Activity, check to see if the user is currently signed in:
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
         }
     }
 }
