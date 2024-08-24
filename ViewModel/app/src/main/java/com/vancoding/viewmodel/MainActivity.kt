a1d3068e2c185489c3ee5767c94f4e72d39ce8c2
Joshlanuevo <lanuevo68@gmail.com>
2024-08-24 14:11:59 +0800

    modify ui

--- ViewModel/app/src/main/java/com/vancoding/viewmodel/MainActivity.kt
+++ ViewModel/app/src/main/java/com/vancoding/viewmodel/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.vancoding.viewmodel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel.getUserData()
+        mainViewModel.users.observe(this, Observer { usersList ->
+            usersList?.forEach {
+                Log.i("MyTag", "name is ${it.name}")
+            }
+        })
     }
 }
