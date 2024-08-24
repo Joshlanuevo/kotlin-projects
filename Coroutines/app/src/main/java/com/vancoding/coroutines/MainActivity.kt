987cb4a7cbdf4a5ca6242a3671e0f859eaa2ade9
Joshlanuevo <lanuevo68@gmail.com>
2024-08-24 12:20:12 +0800

    Structured Concurrency

--- Coroutines/app/src/main/java/com/vancoding/coroutines/MainActivity.kt
+++ Coroutines/app/src/main/java/com/vancoding/coroutines/MainActivity.kt
@@ -35,7 +35,8 @@ class MainActivity : AppCompatActivity() {
         }
         btnDownloadUserData.setOnClickListener {
             CoroutineScope(Dispatchers.Main).launch {
-                tvUserMessage.text = UserDataManager().getTotalUserCount().toString()
+                tvUserMessage.text = UserDataManager2().getTotalUserCount().toString()
+//                tvUserMessage.text = UserDataManager().getTotalUserCount().toString()
 //                downloadUserData()
             }
         }
