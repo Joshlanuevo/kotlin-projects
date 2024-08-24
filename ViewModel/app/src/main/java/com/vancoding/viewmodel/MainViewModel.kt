5280d7d23a32d3190558bfd983b630630d20b7a7
Joshlanuevo <lanuevo68@gmail.com>
2024-08-24 14:03:55 +0800

    modify viewmodel

--- ViewModel/app/src/main/java/com/vancoding/viewmodel/MainViewModel.kt
+++ ViewModel/app/src/main/java/com/vancoding/viewmodel/MainViewModel.kt
@@ -1,17 +1,25 @@
 package com.vancoding.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.vancoding.viewmodel.model.User
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
-
+    private var mRepository = MainRepository()
+    var users : MutableLiveData<List<User>?> = MutableLiveData()
 
     fun getUserData() {
         viewModelScope.launch {
-            // write some code
+            var result : List<User>? = null
+            withContext(Dispatchers.IO){
+                result = mRepository.getUsers()
+            }
+            users.value = result
         }
     }
 }
